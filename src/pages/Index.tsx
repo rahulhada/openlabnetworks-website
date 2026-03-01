c3ead70470a36d3b9150a97f531fc0d2c7feb053
rahulhada <hada.rahul@gmail.com>
2026-03-01 16:45:21 +0530

    keep minimum

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,8 +9,7 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Navbar />
       <Hero />
-      <TrainingSection />
-      <FeaturesSection />
+      <BlogSection/>
       <Footer />
     </div>
   );
